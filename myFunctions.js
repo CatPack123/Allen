3f345e0bc724c2b73f1026618d2898c915dcfcaa
CatPack123 <49885572+CatPack123@users.noreply.github.com>
2019-04-22 18:22:56 -0400

    Add files via upload

---
+++ myFunctions.js
@@ -0,0 +1,12 @@
+/*Student's First and Last Name
+Current Date
+My Function List
+*/
+
+//Thank you alert
+function tyAlert() {
+alert("Thank you for your interest in our mission of Pet Rescue Club. We will contact you soon.");
+}
+
+
+
